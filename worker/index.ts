7d81269aa6fceffbb75df9dc179332f3a99c8790
Silent Shout <161982876+SilentShout42@users.noreply.github.com>
2026-05-04 21:39:03 -0400

    remove redundant redirect

--- worker/index.ts
+++ worker/index.ts
@@ -1,10 +1,5 @@
 export default {
-  async fetch(request: Request): Promise<Response> {
-    const url = new URL(request.url);
-    if (url.protocol === 'http:') {
-      url.protocol = 'https:';
-      return Response.redirect(url.toString(), 301);
-    }
+  async fetch(): Promise<Response> {
     return new Response('Not found', { status: 404 });
   },
 } satisfies ExportedHandler;
